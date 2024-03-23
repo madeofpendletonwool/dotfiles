a5c7a00ad7d187cd8a7b0915e5e590a84c35d911
madeofpendletonwool <collinp@collinpendleton.com>
2024-03-23 09:27:39 -0500

    29 current  2024-03-23 09:27:35  23.11.5541.56528ee42526  6.1.82                          *

--- nixos/collinp-framework.nix
+++ nixos/collinp-framework.nix
@@ -134,6 +134,7 @@
     libnotify
   ];
 
+  programs.hyprland.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
